20bdc6e20d8947f465df6ad0ef105a78b0022543
Rizchel Wood <rizchelwood@gmail.com>
2019-09-04 17:32:01 -0600

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,6 +13,7 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
+  console.log(window)
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
