e6da314c069660a8ccb0e9a54e0253ccab48a79c
Rizchel Wood <rizchelwood@gmail.com>
2019-09-09 12:01:37 -0600

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -13,7 +13,6 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  console.log(window)
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
